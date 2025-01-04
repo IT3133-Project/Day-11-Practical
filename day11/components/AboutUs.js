f4ff02ecf55ae172cfaab4c9d344e8ebb575e16b
Udara Nalawansa <udarakavishka13@gmail.com>
2025-01-04 12:43:23 +0530

    feat: Enhance AboutUs component with improved styling and ScrollView behavior

--- day11/components/AboutUs.js
+++ day11/components/AboutUs.js
@@ -1,10 +1,10 @@
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { Divider, PaperProvider } from "react-native-paper";
 
 export default function AboutUs(){
     return(
         <PaperProvider>
-            <ScrollView>
+            <ScrollView contentContainerStyle={styles.scrollView}>
                 <View>
                     <Text variant="headlineLarge">History</Text>
                     <Divider/>
@@ -15,5 +15,34 @@ export default function AboutUs(){
 }
 
 const styles=StyleSheet.create({
-    
+    container: {
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        justifyContent: 'space-between',
+        padding: 10
+    },
+    imagepad: {
+        padding: 8,
+        alignItems: 'center',
+        flex: 2,
+        marginBottom: 10
+    },
+    image: {
+        width: '80%',
+        height: 70,
+    },
+    body: {
+        padding: 8,
+        textAlign: 'justify',
+        flex: 6,
+    },
+    scrollView: {
+        flexGrow: 1, // Ensures the ScrollView behaves correctly
+    },
+    footer: {
+        flex: 2,
+        width: "100%",
+        alignItems: 'center',
+        marginTop: 10
+    }
 })
