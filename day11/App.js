ff54d15a7fa5122128ab98c8c7c0d08c6ad2ae8c
Udara Nalawansa <udarakavishka13@gmail.com>
2025-01-04 12:16:47 +0530

    feat: Refactor App component to include SafeAreaView and StatusBar for improved layout and user experience

--- day11/App.js
+++ day11/App.js
@@ -1,43 +1,56 @@
-import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Platform, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Home from './components/Home';
 import ContactUs from './components/ContactUs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ScrollView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
+
 function RootStack() {
   return (
-    <Stack.Navigator initialRouteName="Home"  screenOptions={{
-      headerStyle: { backgroundColor: 'lightblue' },
-    }}>
-      <Stack.Screen name="Home" component={Home} options={{ title: "Welcome" }} />
-      <Stack.Screen name="contact" component={ContactUs} options={{ title: "Contact Us" }} />
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: { backgroundColor: 'lightblue' },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ title: 'Welcome' }}
+      />
+      <Stack.Screen
+        name="Contact"
+        component={ContactUs}
+        options={{ title: 'Contact Us' }}
+      />
     </Stack.Navigator>
   );
 }
 
+
 export default function App() {
   return (
-
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <RootStack />
-      </NavigationContainer>
-    </SafeAreaProvider>
-
-
-
-
+    <PaperProvider>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.safeArea}>
+          <NavigationContainer>
+            <RootStack />
+          </NavigationContainer>
+          <StatusBar style="auto" />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+  },
 });
